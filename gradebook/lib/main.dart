bfff3cb5fdfc91275bfa8820ff02b5aeeafb96e5
MichaelM <mercierm@plu.edu>
2020-10-27 11:07:39 -0700

    firebase setup

--- gradebook/lib/main.dart
+++ gradebook/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gradebook/pages/login.dart';
 import 'package:gradebook/pages/sign_up.dart';
 import 'package:gradebook/pages/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main(){
 runApp(Gradebook());
@@ -17,6 +18,8 @@ class Gradebook extends StatefulWidget {
 }
 
 class _GradebookState extends State<Gradebook>{
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
 
 
   @override
