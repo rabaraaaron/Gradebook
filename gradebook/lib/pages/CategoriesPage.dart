6876d6dde0afe17f55ccd56586dcd6ab55ee24c2
Aaron Rabara <rabara777@icloud.com>
2020-11-09 23:19:30 -0800

    Made categories slidable, and formatted the page to fit theme of the app

--- gradebook/lib/pages/CategoriesPage.dart
+++ gradebook/lib/pages/CategoriesPage.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:gradebook/services/auth_service.dart';
@@ -14,11 +15,188 @@ class _CategoriesPageState extends State<CategoriesPage> {
   Map categoriesData = new HashMap<String, List<String>>();
   final GlobalKey scaffoldKey = new GlobalKey();
   final SlidableController slidableController = new SlidableController();
-
   int weight = 20;
+
+
+
+
+
+
+  List<ExpansionTile> expansionList = [];
+
 
   @override
   Widget build(BuildContext context) {
+
+    FlatButton addButton = FlatButton(
+      color: Colors.blue,
+      child: Text(
+        "Add Item",
+        style: Theme.of(context).textTheme.headline1,
+      ),
+    );
+
+    ExpansionTile e1 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      title: Text(
+          "Assignments 25%",
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("Assignment 1", textScaleFactor: 1.5,),
+              Text("Assignment 2", textScaleFactor: 1.5,),
+              Text("Assignment 3", textScaleFactor: 1.5,),
+            ],),
+        )
+      ],
+    );
+
+    ExpansionTile e2 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      title: Text(
+        "Exams 25%",
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("Exam 1", textScaleFactor: 1.5,),
+              Text("Exam 2", textScaleFactor: 1.5,),
+              Text("Exam 4", textScaleFactor: 1.5,),
+              Text("Exam 5", textScaleFactor: 1.5,),
+              Text("Exam 2", textScaleFactor: 1.5,),
+              Text("Exam 4", textScaleFactor: 1.5,),
+              Text("Exam 5", textScaleFactor: 1.5,),
+              Text("Exam 2", textScaleFactor: 1.5,),
+              Text("Exam 4", textScaleFactor: 1.5,),
+              Text("Exam 5", textScaleFactor: 1.5,),
+            ],
+          ),
+        )
+      ],
+    );
+
+    ExpansionTile e3 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      //expandedCrossAxisAlignment,
+      title: Text(
+        "Project 10%",
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("Assignment 1", textScaleFactor: 1.5,),
+              Text("Assignment 2", textScaleFactor: 1.5,),
+              Text("Assignment 3", textScaleFactor: 1.5,),
+            ],),
+        )
+      ],
+    );
+
+    ExpansionTile e4 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      title: Text(
+          "Quizzes 15%",
+          style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("Quiz 1", textScaleFactor: 1.5,),
+              Text("Quiz 2", textScaleFactor: 1.5,),
+              Text("Quiz 3", textScaleFactor: 1.5,),
+              Text("Quiz 4", textScaleFactor: 1.5,),
+            ],
+          ),
+        )
+      ],
+    );
+
+    ExpansionTile e5 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      //expandedCrossAxisAlignment,
+      title: Text(
+          "Homework 20%" ,
+          style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("HW 1", textScaleFactor: 1.5,),
+              Text("HW 2", textScaleFactor: 1.5,),
+              Text("HW 3", textScaleFactor: 1.5,),
+            ],),
+        )
+      ],
+    );
+
+    ExpansionTile e6 = ExpansionTile(
+      backgroundColor: Colors.grey[800],
+      title: Text(
+          "Other 5%",
+          style: Theme.of(context).textTheme.headline2,
+      ),
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                child: addButton,
+              ),
+              Text("test ", textScaleFactor: 1.5,),
+              Text("test ", textScaleFactor: 1.5,),
+              Text("test ", textScaleFactor: 1.5,),
+              Text(" ", textScaleFactor: 1.5,),
+            ],
+          ),
+        )
+      ],
+    );
+
+    expansionList.clear();
+
+    expansionList.add(e1);
+    expansionList.add(e2);
+    expansionList.add(e3);
+    expansionList.add(e4);
+    expansionList.add(e5);
+    expansionList.add(e6);
+
     return Scaffold(
       key: scaffoldKey,
       drawer: MenuDrawer(),
@@ -57,192 +235,35 @@ class _CategoriesPageState extends State<CategoriesPage> {
         ],
       ),
 
-      body: ListView(
-        children: [
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            title: Text("Assignments      25%" , textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("Assignment 1", textScaleFactor: 1.5,),
-                    Text("Assignment 2", textScaleFactor: 1.5,),
-                    Text("Assignment 3", textScaleFactor: 1.5,),
-                ],),
-              )
-            ],),
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            title: Text("Exams               25%", textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("Exam 1", textScaleFactor: 1.5,),
-                    Text("Exam 2", textScaleFactor: 1.5,),
-                    Text("Exam 4", textScaleFactor: 1.5,),
-                    Text("Exam 5", textScaleFactor: 1.5,),
-                    Text("Exam 2", textScaleFactor: 1.5,),
-                    Text("Exam 4", textScaleFactor: 1.5,),
-                    Text("Exam 5", textScaleFactor: 1.5,),
-                    Text("Exam 2", textScaleFactor: 1.5,),
-                    Text("Exam 4", textScaleFactor: 1.5,),
-                    Text("Exam 5", textScaleFactor: 1.5,),
-                  ],
-                ),
-              )
-          ],
+      body: ListView.separated(
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.white,
+            indent: 25.0,
+            endIndent: 25.0,
           ),
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            //expandedCrossAxisAlignment,
-            title: Text("Project               10%" , textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("Assignment 1", textScaleFactor: 1.5,),
-                    Text("Assignment 2", textScaleFactor: 1.5,),
-                    Text("Assignment 3", textScaleFactor: 1.5,),
-                  ],),
-              )
-            ],),
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            title: Text("Quizzes             15%", textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("Quiz 1", textScaleFactor: 1.5,),
-                    Text("Quiz 2", textScaleFactor: 1.5,),
-                    Text("Quiz 3", textScaleFactor: 1.5,),
-                    Text("Quiz 4", textScaleFactor: 1.5,),
-                  ],
-                ),
+        itemCount: expansionList.length,
+        itemBuilder: (context, index) => Container(
+          child: Slidable(
+            controller: slidableController,
+            actionPane: SlidableDrawerActionPane(),
+            actionExtentRatio: .2,
+            secondaryActions: <Widget>[
+              IconSlideAction(
+                color: Colors.transparent,
+                closeOnTap: true,
+                iconWidget: Icon(Icons.more_vert, color: Colors.white, size: 35,),
+                onTap: () => null,
+              ),
+              IconSlideAction(
+                color: Colors.transparent,
+                closeOnTap: true,
+                iconWidget: Icon(Icons.delete, color: Colors.white, size: 35,),
               )
             ],
+            child: expansionList[index],
           ),
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            //expandedCrossAxisAlignment,
-            title: Text("Homework        20%" , textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("HW 1", textScaleFactor: 1.5,),
-                    Text("HW 2", textScaleFactor: 1.5,),
-                    Text("HW 3", textScaleFactor: 1.5,),
-                  ],),
-              )
-            ],),
-          ExpansionTile(
-            backgroundColor: Colors.grey[800],
-            title: Text("Other                  5%", textScaleFactor: 2),
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    FlatButton(child: Text("Add item",),),
-                    Text("test ", textScaleFactor: 1.5,),
-                    Text("test ", textScaleFactor: 1.5,),
-                    Text("test ", textScaleFactor: 1.5,),
-                    Text(" ", textScaleFactor: 1.5,),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ]
+        ),
       )
-        //====================================
-      //   ListView.separated(
-      //   separatorBuilder: (context, index) =>
-      //       Divider(
-      //         color: Colors.white,
-      //         indent: 25.0,
-      //         endIndent: 25.0,
-      //       ),
-      //   itemCount: categoriesStrings.length,
-      //   itemBuilder: (context, index) =>
-      //       Slidable(
-      //         controller: slidableController,
-      //         actionPane: SlidableDrawerActionPane(),
-      //         actionExtentRatio: .2,
-      //         secondaryActions: [
-      //           IconSlideAction(
-      //             color: Colors.transparent,
-      //             closeOnTap: true,
-      //             iconWidget: Icon(Icons.more_vert, color: Colors.white, size: 35,),
-      //             onTap: () => null,
-      //           ),
-      //           IconSlideAction(
-      //             color: Colors.transparent,
-      //             closeOnTap: true,
-      //             iconWidget: Icon(Icons.delete, color: Colors.white, size: 35,),
-      //           )
-      //         ],
-      //         child: Container(
-      //             width: MediaQuery
-      //                 .of(context)
-      //                 .size
-      //                 .width,
-      //             padding: EdgeInsets.all(5),
-      //             child: Row(
-      //               mainAxisAlignment: MainAxisAlignment.start,
-      //               children: [
-      //                 Container(
-      //                   child: Icon(
-      //                       Icons.chevron_right,
-      //                       size: 50,
-      //                     ),
-      //                   ),
-      //                 Expanded(
-      //                   child: GestureDetector(
-      //                     onTap: () {
-      //                       //Todo: make items in categories drop down
-      //                     },
-      //                     child: new Padding(
-      //                       padding: new EdgeInsets.all(20.0),
-      //                       child: Text(
-      //                         "${categoriesStrings[index]}",
-      //                         style: Theme
-      //                             .of(context)
-      //                             .textTheme
-      //                             .headline2,
-      //                       ),
-      //                     ),
-      //                   ),
-      //                 ),
-      //                 Text("20%", textScaleFactor: 2, style: Theme
-      //                     .of(context)
-      //                     .textTheme
-      //                     .headline3,)
-      //                 //================================================
-      //               ],
-      //             )
-      //         ),
-      //       ),
-      // ),
     );
   }
 
