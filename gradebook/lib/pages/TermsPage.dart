3ad20284b0039428a21b2d31152e14214f97d164
Aaron Rabara <rabara777@icloud.com>
2020-11-22 22:18:31 -0800

    Added changing of term icons. Formatted some code

--- gradebook/lib/pages/TermsPage.dart
+++ gradebook/lib/pages/TermsPage.dart
@@ -9,202 +9,6 @@ import 'package:gradebook/services/user_service.dart';
 import 'package:gradebook/utils/menuDrawer.dart';
 import 'package:provider/provider.dart';
 
-// class TermsPage extends StatefulWidget {
-//   @override
-//   _TermsPageState createState() => _TermsPageState();
-// }
-//
-// class _TermsPageState extends State<TermsPage> {
-//   List<String> terms = ["Fall 2020", "J-term 2021", "Spring 2021", "Summer 2021"];
-//   // List<Term> terms = TermService().terms;
-//   final SlidableController slidableController = SlidableController();
-//   final GlobalKey scaffoldKey = new GlobalKey();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     Random rand = new Random();
-//     return Scaffold(
-//       key: scaffoldKey,
-//       drawer: MenuDrawer(),
-//       appBar: AppBar(
-//         title: Text(
-//           "Terms",
-//           style: Theme.of(context).textTheme.headline1,
-//         ),
-//         centerTitle: true,
-//         leading: Builder(
-//           builder: (context) =>
-//               Center(
-//                 child: IconButton(
-//                     icon: Icon(Icons.menu_sharp, color: Colors.white,),
-//                     iconSize: 30,
-//                     onPressed: (){
-//                       Scaffold.of(context).openDrawer();
-//                     }),
-//               ),
-//         ),
-//         actions: [IconButton(icon: Icon(Icons.add_sharp), iconSize: 35, color: Colors.white,
-//             onPressed: () async{
-//               await showDialog(
-//                 context: context,
-//                 builder: (BuildContext context) =>
-//                     NewTermsPopUp(context, terms),
-//               );
-//               setState(() {});
-//             }
-//         )],
-//       ),
-//       body: ListView.separated(
-//         separatorBuilder: (context, index) => Divider(
-//           color: Colors.white,
-//           indent: 25.0,
-//           endIndent: 25.0,
-//         ),
-//         itemCount: terms.length,
-//         itemBuilder: (context, index) => Container(
-//           child: Slidable(
-//             controller: slidableController,
-//             actionPane: SlidableDrawerActionPane(),
-//             actionExtentRatio: .2,
-//             secondaryActions: <Widget>[
-//               IconSlideAction(
-//                 color: Colors.transparent,
-//                 closeOnTap: true,
-//                 iconWidget: Icon(Icons.more_vert, color: Colors.white, size: 35,),
-//                 onTap: () => null,
-//               ),
-//               IconSlideAction(
-//                 color: Colors.transparent,
-//                 closeOnTap: true,
-//                 iconWidget: Icon(Icons.delete, color: Colors.white, size: 35,),
-//               )
-//             ],
-//             child: Container(
-//                 width: MediaQuery.of(context).size.width,
-//                 padding: EdgeInsets.all(10),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.start,
-//                   children: [
-//                     Container(
-//                       padding: EdgeInsets.all(10.0),
-//                       child: IconButton(
-//                         icon: Icon(Icons.ac_unit),
-//                         iconSize: 35.0,
-//                         onPressed: (){
-//                           print("Icons pressed");
-//                         },
-//                       ),
-//                     ),
-//                     Expanded(
-//                       child: GestureDetector(
-//                         onTap: (){
-//                           Navigator.pushNamed(context, "/Home");
-//                         },
-//                         child: new Padding(
-//                           padding: new EdgeInsets.all(20.0),
-//                           child: Text(
-//                             "${terms[index]}",
-//                             style: Theme.of(context).textTheme.headline2,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     Text("4.0", style: Theme.of(context).textTheme.headline3, textScaleFactor: 2,),
-//                   ],
-//                 )
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-//
-// class NewTermsPopUp extends StatefulWidget {
-//   var c;
-//   var termsList;
-//
-//   NewTermsPopUp(context, terms){
-//     c = context;
-//     termsList = terms;
-//   }
-//   @override
-//   _NewTermsPopUpState createState() => _NewTermsPopUpState(c, termsList);
-// }
-//
-// class _NewTermsPopUpState extends State<NewTermsPopUp> {
-//   var thisContext;
-//   var thisTerms;
-//   var termYear;
-//   var addedTerm;
-//
-//   _NewTermsPopUpState(context, terms){
-//     thisContext = context;
-//     thisTerms = terms;
-//   }
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     TermService term = new TermService();
-//
-//     List<String> listOfTermsRaw = ["Fall", "Winter", "Spring", "Summer"];
-//     List<DropdownMenuItem> listOfTerms = [];
-//     for(var l = 0; l < listOfTermsRaw.length; l++){
-//       listOfTerms.insert(0, DropdownMenuItem(
-//         child: Text("${listOfTermsRaw[l]}"),
-//         value: listOfTermsRaw[l],
-//       ));
-//     }
-//     List<DropdownMenuItem> listOfYears = [];
-//     for(var i = 2015; i <= DateTime.now().year; i++){
-//       listOfYears.insert(0, DropdownMenuItem(
-//         child: Text("$i"),
-//         value: i,
-//       ));
-//     }
-//
-//     return AlertDialog(
-//         title: Text("Add a new Term", style: Theme.of(context).textTheme.headline2,),
-//         content: SizedBox(
-//           child: Form(
-//               child: Column(children: [
-//                 DropdownButton(
-//                   hint: Text("Term", style: Theme.of(context).textTheme.headline3,),
-//                   value: addedTerm,
-//                   items: listOfTerms,
-//                   onChanged: (newTerm) {
-//                     setState(() {
-//                       addedTerm = newTerm;
-//                     });
-//                   },
-//                   isExpanded: true,
-//                 ),
-//                 DropdownButton(
-//                   hint: Text("Year", style: Theme.of(context).textTheme.headline3,),
-//                   onChanged: (newYear) {
-//                     setState(() {
-//                       termYear = newYear;
-//                     });
-//                   },
-//                   value: termYear,
-//                   isExpanded: true,
-//                   items: listOfYears,
-//                 ),
-//                 RaisedButton(onPressed: ()async{
-//                   thisTerms.insert(0, "$addedTerm" + " " + "$termYear");
-//                   await term.addTerm(addedTerm, termYear);
-//                   Navigator.pop(context);
-//                 }, child: Text("Add",),)
-//               ])),
-//           width: 100,
-//           height: 175,
-//         ));
-//   }
-// }
-
 class TermsPage extends StatefulWidget {
   @override
   _TermsPageState createState() => _TermsPageState();
@@ -228,11 +32,26 @@ class TermsList extends StatefulWidget {
 class _TermsListState extends State<TermsList> {
   final SlidableController slidableController = SlidableController();
   final GlobalKey scaffoldKey = new GlobalKey();
+  List<Image> seasonIcons = [];
+
 
   @override
   Widget build(BuildContext context) {
     final terms = Provider.of<List<Term>>(context);
-    Random rand = new Random();
+    seasonIcons.clear();
+    double scale = 9.5;
+    for(int i = 0; i < terms.length; i++){
+      if(terms[i].name == "Fall"){
+        seasonIcons.add(Image.asset('assets/Fall_icon.png', scale: scale, color: Colors.white,));
+      } else if(terms[i].name == "Winter"){
+        seasonIcons.add(Image.asset('assets/Winter.png', scale: scale, color: Colors.white,));
+      } else if(terms[i].name == "Spring"){
+        seasonIcons.add(Image.asset('assets/Flower.png', scale: scale, color: Colors.white,));
+      } else{
+        seasonIcons.add(Image.asset('assets/Sun.png', scale: scale, color: Colors.white));
+      }
+    }
+
     return Scaffold(
       key: scaffoldKey,
       drawer: MenuDrawer(),
@@ -305,26 +124,7 @@ class _TermsListState extends State<TermsList> {
                   showDialog(
                       context: context,
                       builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text(
-                            "Delete Term",
-                            style: Theme.of(context).textTheme.headline2,
-                          ),
-                          content: Text("Are you sure you want to delete this term?",),
-                          actions: <Widget>[
-                            FlatButton(
-                                onPressed: () {
-                                  TermService().deleteTerm("${terms[index].name}", terms[index].year);
-                                  Navigator.pop(context);
-                                  },
-                                child: Text("Delete", textScaleFactor: 1.25,),
-                            ),
-                            FlatButton(
-                              onPressed: (){Navigator.pop(context);},
-                              child: Text("Close", textScaleFactor: 1.25,),
-                            ),
-                          ],
-                        );
+                        return DeleteConfirmation(terms, index);
                       },
                   );
                 },
@@ -338,13 +138,7 @@ class _TermsListState extends State<TermsList> {
                   children: [
                     Container(
                       padding: EdgeInsets.all(10.0),
-                      child: IconButton(
-                        icon: Icon(Icons.ac_unit),
-                        iconSize: 35.0,
-                        onPressed: () {
-                          print("Icons pressed");
-                        },
-                      ),
+                      child: seasonIcons[index],
                     ),
                     Expanded(
                       child: GestureDetector(
@@ -471,4 +265,39 @@ class _NewTermsPopUpState extends State<NewTermsPopUp> {
           height: 175,
         ));
   }
+}
+
+class DeleteConfirmation extends StatelessWidget{
+  List<Term> terms;
+  int index;
+
+  DeleteConfirmation(List<Term> list, int i){
+    terms = list;
+    index = i;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        "Delete Term",
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      content: Text("Are you sure you want to delete this term?",),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            TermService().deleteTerm("${terms[index].name}", terms[index].year);
+            Navigator.pop(context);
+          },
+          child: Text("Delete", textScaleFactor: 1.25,),
+        ),
+        FlatButton(
+          onPressed: (){Navigator.pop(context);},
+          child: Text("Close", textScaleFactor: 1.25,),
+        ),
+      ],
+    );
+  }
+
 }
