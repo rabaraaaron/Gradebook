e376f52a692583e2b751fcfa9eb25dbbfdb81b28
Aaron Rabara <rabara777@icloud.com>
2020-11-22 19:09:28 -0800

    Changed some filenames to be Captalized. Added a popup for term deletion. Added 'drop lowest score' option when creating categories

--- gradebook/lib/pages/CategoriesPage.dart
+++ gradebook/lib/pages/CategoriesPage.dart
@@ -11,7 +11,13 @@ class CategoriesPage extends StatefulWidget {
 }
 
 class _CategoriesPageState extends State<CategoriesPage> {
-  List<String> categoriesStrings = ["Assignments", "Homework", "Quizzes", "Exams","Other"];
+  List<String> categoriesStrings = [
+    "Assignments",
+    "Homework",
+    "Quizzes",
+    "Exams",
+    "Other"
+  ];
   Map categoriesData = new HashMap<String, List<String>>();
   final GlobalKey scaffoldKey = new GlobalKey();
   final SlidableController slidableController = new SlidableController();
@@ -23,20 +29,25 @@ class _CategoriesPageState extends State<CategoriesPage> {
 
   @override
   Widget build(BuildContext context) {
-
     FlatButton addButton = FlatButton(
       color: Colors.blue,
       child: Text(
         "Add Item",
-        style: Theme.of(context).textTheme.headline1,
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline1,
       ),
     );
 
     ExpansionTile e1 = ExpansionTile(
       backgroundColor: Colors.grey[800],
       title: Text(
-          "Assignments 25%",
-        style: Theme.of(context).textTheme.headline2,
+        "Assignments 25%",
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -59,7 +70,10 @@ class _CategoriesPageState extends State<CategoriesPage> {
       backgroundColor: Colors.grey[800],
       title: Text(
         "Exams 25%",
-        style: Theme.of(context).textTheme.headline2,
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -91,7 +105,10 @@ class _CategoriesPageState extends State<CategoriesPage> {
       //expandedCrossAxisAlignment,
       title: Text(
         "Project 10%",
-        style: Theme.of(context).textTheme.headline2,
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -113,8 +130,11 @@ class _CategoriesPageState extends State<CategoriesPage> {
     ExpansionTile e4 = ExpansionTile(
       backgroundColor: Colors.grey[800],
       title: Text(
-          "Quizzes 15%",
-          style: Theme.of(context).textTheme.headline2,
+        "Quizzes 15%",
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -139,8 +159,11 @@ class _CategoriesPageState extends State<CategoriesPage> {
       backgroundColor: Colors.grey[800],
       //expandedCrossAxisAlignment,
       title: Text(
-          "Homework 20%" ,
-          style: Theme.of(context).textTheme.headline2,
+        "Homework 20%",
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -162,8 +185,11 @@ class _CategoriesPageState extends State<CategoriesPage> {
     ExpansionTile e6 = ExpansionTile(
       backgroundColor: Colors.grey[800],
       title: Text(
-          "Other 5%",
-          style: Theme.of(context).textTheme.headline2,
+        "Other 5%",
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline2,
       ),
       children: [
         Container(
@@ -194,82 +220,107 @@ class _CategoriesPageState extends State<CategoriesPage> {
     expansionList.add(e6);
 
     return Scaffold(
-      key: scaffoldKey,
-      drawer: MenuDrawer(),
-      appBar: AppBar(
-        title: Text(
-          "Categories",
-          style: Theme
-              .of(context)
-              .textTheme
-              .headline1,
-        ),
-        centerTitle: true,
-        leading:
+        key: scaffoldKey,
+        drawer: MenuDrawer(),
+        appBar: AppBar(
+          title: Text(
+            "Categories",
+            style: Theme
+                .of(context)
+                .textTheme
+                .headline1,
+          ),
+          centerTitle: true,
+          leading:
           Builder(
             builder: (context) =>
                 Center(
                   child: IconButton(
                       icon: Icon(Icons.menu_sharp, color: Colors.white,),
                       iconSize: 30,
-                      onPressed: (){
+                      onPressed: () {
                         Scaffold.of(context).openDrawer();
                       }),
                 ),
           ),
-        actions: [
-          IconButton(icon: Icon(Icons.add), iconSize: 35, color: Colors.white,
-              onPressed: () async {
-                await showDialog(
-                  context: context,
-                  builder: (BuildContext context) =>
-                      newCategoriesPopUp(context, categoriesStrings),
-                );
-                setState(() {});
-              }
-          )
-        ],
-      ),
-
-      body: ListView.separated(
-          separatorBuilder: (context, index) => Divider(
-            color: Colors.white,
-            indent: 25.0,
-            endIndent: 25.0,
-          ),
-        itemCount: expansionList.length,
-        itemBuilder: (context, index) => Container(
-          child: Slidable(
-            controller: slidableController,
-            actionPane: SlidableDrawerActionPane(),
-            actionExtentRatio: .2,
-            secondaryActions: <Widget>[
-              IconSlideAction(
-                color: Colors.transparent,
-                closeOnTap: true,
-                iconWidget: Icon(Icons.more_vert, color: Colors.white, size: 35,),
-                onTap: () => null,
-              ),
-              IconSlideAction(
-                color: Colors.transparent,
-                closeOnTap: true,
-                iconWidget: Icon(Icons.delete, color: Colors.white, size: 35,),
-              )
-            ],
-            child: expansionList[index],
-          ),
+          actions: [
+            IconButton(icon: Icon(Icons.add), iconSize: 35, color: Colors.white,
+                onPressed: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                         NewCategoriesPopUp(categoriesStrings),
+                  );
+                  setState(() {});
+                }
+            )
+          ],
         ),
-      )
+
+        body: ListView.separated(
+          separatorBuilder: (context, index) =>
+              Divider(
+                color: Colors.white,
+                indent: 25.0,
+                endIndent: 25.0,
+              ),
+          itemCount: expansionList.length,
+          itemBuilder: (context, index) =>
+              Container(
+                child: Slidable(
+                  controller: slidableController,
+                  actionPane: SlidableDrawerActionPane(),
+                  actionExtentRatio: .2,
+                  secondaryActions: <Widget>[
+                    IconSlideAction(
+                      color: Colors.transparent,
+                      closeOnTap: true,
+                      iconWidget: Icon(
+                        Icons.more_vert, color: Colors.white, size: 35,),
+                      onTap: () => null,
+                    ),
+                    IconSlideAction(
+                      color: Colors.transparent,
+                      closeOnTap: true,
+                      iconWidget: Icon(
+                        Icons.delete, color: Colors.white, size: 35,),
+                    )
+                  ],
+                  child: expansionList[index],
+                ),
+              ),
+        )
     );
   }
+}
 
-  Widget newCategoriesPopUp(BuildContext c, List<String> categories) {
+
+class NewCategoriesPopUp extends StatefulWidget {
+  List<String> c = [];
+
+  NewCategoriesPopUp(List<String> categories){
+    c = categories;
+  }
+  @override
+  _NewCategoriesPopUpState createState() => _NewCategoriesPopUpState(c);
+}
+
+class _NewCategoriesPopUpState extends State<NewCategoriesPopUp> {
+  List<String> c = [];
+  bool checked = false;
+
+
+  _NewCategoriesPopUpState(List<String> categories){
+    c = categories;
+  }
+  @override
+  Widget build(BuildContext context) {
     TextEditingController categoryTitleController = new TextEditingController();
     TextEditingController categoryWeightController = new TextEditingController();
 
     return AlertDialog(
         title: Text("Add a new Category", style: Theme
-            .of(c)
+            .of(context)
             .textTheme
             .headline2,),
         content: SizedBox(
@@ -289,41 +340,39 @@ class _CategoriesPageState extends State<CategoriesPage> {
                     labelText: 'Weight',
                   ),
                 ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: checked,
+                      onChanged: (updateChecked){
+                        setState(() {
+                          checked = updateChecked;
+                        });
+                      },
+
+                    ),
+                    Text("Drop Lowest Score?"),
+                  ],
+                ),
                 RaisedButton(
                     onPressed: () {
                       if (int.parse(categoryWeightController.text) is int) {
-                        categories.insert(0, categoryTitleController.text);
+                        c.insert(0, categoryTitleController.text);
                         print(categoryWeightController.text);
                       } else {
                         print("Input is invalid");
                       }
-                      Navigator.pop(c);
+                      Navigator.pop(context);
                     },
-                    child: Text("Submit")
+                    child: Text("Add")
                 )
               ])
           ),
           width: 100,
-          height: 175,
+          height: 225,
         )
     );
   }
 }
 
 
-class Categories{
-  String categoryName;
-  List<String> categoryItems = new List<String>();
-
-  Categories(String categoryName){
-    this.categoryName = categoryName;
-  }
-
-  void add(String itemName){
-    categoryItems.add(itemName);
-  }
-
-  void delete(String itemName){
-    categoryItems.remove(itemName);
-  }
-}
--- gradebook/lib/services/term_service.dart
+++ gradebook/lib/services/term_service.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:gradebook/model/term.dart';
-import 'package:gradebook/model/user.dart';
+import 'package:gradebook/model/Term.dart';
+import 'package:gradebook/model/User.dart';
 import 'package:provider/provider.dart';
 import 'user_service.dart';
 
