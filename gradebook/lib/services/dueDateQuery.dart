baa7b482e3bf842f77324d2831f4bb0e7329d00c
rabaraaq <rabaraaq@plu.edu>
2021-03-26 22:46:08 -0700

    Commented error in dueDateQuery.dart

--- gradebook/lib/services/dueDateQuery.dart
+++ gradebook/lib/services/dueDateQuery.dart
@@ -4,8 +4,8 @@ import 'package:gradebook/model/Assessment.dart';
 
 class DueDateQuery {
 
-  Future<List<Assessment>> getAssesmentsDue() async {
-   DateTime datesToDisplay = DateTime.now().add
-  }
+  // Future<List<Assessment>> getAssesmentsDue() async {
+  //  DateTime datesToDisplay = DateTime.now().add
+  // }
 
 }
