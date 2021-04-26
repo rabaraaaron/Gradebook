25ada57c5c6c2b3b3b6b301c2c342088eea7964d
Mohammad Almutawa <almutawa@plu.edu>
2021-04-26 01:18:13 -0700

    changed the icone of the app-theme tile in the settings page

--- gradebook/lib/pages/Settings/NotificationsSettingsTile.dart
+++ gradebook/lib/pages/Settings/NotificationsSettingsTile.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
 class NotificationsSettingsTile extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TextStyle textStyle_H5 = Theme.of(context).textTheme.headline5;
+
+
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 20, right: 20,),
+      child: ListView(
+      children: [
+        ListTile(
+          title: Text('Allow Notifications', style: textStyle_H5,),
+        )
+      ],
+      )
+    );
   }
 }
